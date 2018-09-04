b3c690dd37f9129697b64a096f8a2b380d3f74f3
michal <michal.golebiowski4@gmail.com>
2018-09-04 15:46:59 +0200

    Add list of issues

--- static/dev/js/main.js
+++ static/dev/js/main.js
@@ -2,5 +2,28 @@ import "../styles/style.scss";
 import $ from "jquery";
 
 $.ajax("/api/issues").then((resp)=>{
-  console.log(resp);
+  resp.forEach((issue, index) => {
+    let status = "";
+    switch(issue.status){
+    case 0:
+      status = "open";
+      break;
+    case 1:
+      status = "pending";
+      break;
+    case 2:
+      status = "closed";
+      break;
+    }
+    const el = $(`
+    <tr>
+      <td scope="row">${index}</td>
+      <td>${issue.title}</td>
+      <td>${issue.description}</td>
+      <td>${status}</td>
+    </tr>
+    `);
+    $("#list").append(el);
+  });
+
 });
